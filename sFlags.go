76c4f7ee1901f55d9e32f553fcc175b232bbbbbd
haltermak <haltermak@sou.edu>
2018-02-11 15:27:51 +0100

    Fixed createFlags to capture all flags

---
+++ sFlags.go
@@ -0,0 +1,44 @@
+package sFlags
+
+import (
+	"strconv"
+	"strings"
+)
+
+func CreateFlags(input string) (string, map[string]string, error) {
+	temp := strings.Fields(input)
+	var err error
+	var output map[string]string
+	output = make(map[string]string)
+	for i := 1; i < len(temp); {
+		if strings.HasPrefix(temp[i], "-") {
+			if i+1 == len(temp) || strings.HasPrefix(temp[i+1], "-") {
+				output[temp[i]] = "true"
+				i++
+			} else {
+				output[temp[i]] = temp[i+1]
+				i += 2
+			}
+		}
+	}
+	//fmt.Println(output) //uncomment to help in debugging
+	return temp[0], output, err
+}
+
+func FlagToInt(fM map[string]string, key string) (int, error) {
+	var err error
+	temp, err := strconv.Atoi(fM[key])
+	return temp, err
+}
+
+func FlagToString(fM map[string]string, key string) (string, error) {
+	var err error
+	return fM[key], err
+}
+
+func FlagToBool(fM map[string]string, key string) (bool, error) {
+	var err error
+	var temp bool
+	temp, err = strconv.ParseBool(fM[key])
+	return temp, err
+}
